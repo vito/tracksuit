6f04df0d54b7c8579d045d9707a020254c8e21bf
Jeff Billimek <jeffrey_k_billimek@homedepot.com>
2016-04-28 14:14:04 -0400

    Adopting the actual tracksuit pipeline from https://github.com/concourse/concourse/blob/develop/ci/pipelines/tracksuit.yml

--- pagination_helpers.go
+++ pagination_helpers.go
@@ -3,6 +3,7 @@ package main
 import "github.com/google/go-github/github"
 
 var publicReposFilter = github.RepositoryListByOrgOptions{Type: "public"}
+var userPurblicReposFilter = github.RepositoryListOptions{Type: "public"}
 var openIssuesFilter = github.IssueListByRepoOptions{State: "open"}
 
 func (syncer *Syncer) reposToSync() ([]github.Repository, error) {
@@ -10,13 +11,14 @@ func (syncer *Syncer) reposToSync() ([]github.Repository, error) {
 
 	var repos []github.Repository
 
+	// TODO: clean-up how these two for loops operates (e.g. checking first by org and then by user)
 	for {
 		resources, resp, err := syncer.GithubClient.Repositories.ListByOrg(
 			syncer.OrganizationName,
 			&options,
 		)
 		if err != nil {
-			return nil, err
+			//return nil, err
 		}
 
 		if len(resources) == 0 {
@@ -34,6 +36,37 @@ func (syncer *Syncer) reposToSync() ([]github.Repository, error) {
 		}
 
 		options.ListOptions.Page = resp.NextPage
+	}
+
+	// if searching by org did not work, try searching by username
+	if len(repos) == 0 {
+		options := userPurblicReposFilter
+
+		for {
+			resources, resp, err := syncer.GithubClient.Repositories.List(
+				syncer.OrganizationName,
+				&options,
+			)
+			if err != nil {
+				return nil, err
+			}
+
+			if len(resources) == 0 {
+				break
+			}
+
+			for _, repo := range resources {
+				if syncer.Repositories.IsEmpty() || syncer.Repositories.Contains(*repo.Name) {
+					repos = append(repos, repo)
+				}
+			}
+
+			if resp.NextPage == 0 {
+				break
+			}
+
+			options.ListOptions.Page = resp.NextPage
+		}
 	}
 
 	return repos, nil
